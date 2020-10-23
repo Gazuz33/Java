4078697e1bbac141c1fc8fda835971c6fbff4331
Andrew <a4tonetak@gmail.com>
2020-10-23 14:48:21 +0300

    added without Nutrisious

---
+++ src/com/company/Cheese.java
@@ -0,0 +1,9 @@
+package com.company;
+public class Cheese extends Food {
+    public Cheese() {
+        super("Сыр");
+    }
+    public void consume() {
+        System.out.println(this + " съеден");
+    }
+}
